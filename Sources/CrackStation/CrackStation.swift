88bdd218cceb577ffd06c9d60396261f881ae3e1
masilamm <masilamm@oregonstate.edu>
2022-12-02 00:52:03 -0800

    Updated lookuptable

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -1,7 +1,6 @@
 import Foundation
 public struct CrackStation: Decrypter {
     private let lookuptable : [String : String]  
-    //Load the dictionary once during object creation  
     public init() {
         do {lookuptable = try GetLookUpTable().loadDictionaryFromDisk()}
         catch{ lookuptable = ["":""]}
@@ -13,13 +12,13 @@ public struct CrackStation: Decrypter {
         {return nil}
     }
    }
-   public class GetLookUpTable {
+   private class GetLookUpTable {
      func loadDictionaryFromDisk() throws -> [String : String] {
         guard let path = Bundle.module.url(forResource: "data", withExtension: "json") else { return [:] }        
         let data = try Data(contentsOf: path)
         let jsonResult = try JSONSerialization.jsonObject(with: data)        
         if let lookupTable: Dictionary = jsonResult as? Dictionary<String, String> {
-            return lookupTable
+            return lookupTable  
          } 
          else {return [:]}
      }
